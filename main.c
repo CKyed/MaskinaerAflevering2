4d7b32a036fd40545aa372771ff4de60aad5c688
StationærPC <christiankyed@hotmail.com>
2021-04-21 16:59:17 +0200

    Viderudviklet på implementering af grundstrukturen i vores spil

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <vss.h>
+//#include <vss.h>
 #include <stdbool.h>
 #include <string.h>
+void printCardDeck();
+void testCardDeck();
 
 
 struct card{
@@ -11,44 +13,93 @@ struct card{
 } card;
 
 struct node{
-   struct card nodeCard;
-   struct Node* next;
+    //Hver node indeholder 3 pointers
+    //En pointer der peger hen på et specifikt kort
+   struct card *nodeCard;
+   //En pointer der peger hen på det næste kort
+   struct Node *nextPointer;
+   //En pointer der peger hen på kortet før
+   struct Node *previousPointer;
+
 }node;
 
-//Et array af 11 pointers til HEAD i hver af de 11 linklister
+//Et array af 7 pointers til HEAD i hver af de 7 linklister
 struct node *headArray[7];
 //Et array med alle vores 52 cord som der er i bunken ved start
 struct card cardArray[52];
 
+void printCardDeck();
+
 int main() {
     printf("Hello, World!\n");
-    return 0;
+    testCardDeck();
+    printCardDeck();
+
 
 }
+    void testCardDeck(){
+        char tempChar="H";
+        for (int i = 0; i < 4; ++i) {
+            if(i==1){
+                tempChar="D";
+            }if(i==2){
+                tempChar="C";
+            }if(i==3){
+                tempChar="S";
+            }
 
-void test(){
+            for (int i = 1; i < 14; ++i) {
+                struct card tempCard;
+                tempCard.rank=i;
+                tempCard.suit=tempChar;
+                tempCard.isFaceUp=true;
+                //TODO Spørg en TA om hvorfor den ikke vil printe vores suit som char. Kan Måske være noget med pointers.
+                printf("rank: %d suit: %c isFaceUp:%d \n",tempCard.rank,tempCard.suit,tempCard.isFaceUp);
+                cardArray[i]=tempCard;
 
-}
+            }
+        }
 
-_Noreturn void print(){
-    bool nextPointer=true;
-    char printCards[2][7];
-    struct node *tempNextPointer[7];
-    for (int i = 0; i < 7; ++i) {
-        //TODO Skal tjekke om den rent faktisk giver pointeren
-        //Her sørger vi for at den første pointer vi starter med peger på HEAD
-        tempNextPointer[i]=headArray[i];
-    }
-
-
-    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7");
-    printf("\n");
-    while(nextPointer){
         for (int i = 0; i < 7; ++i) {
+            //TODO Spørgsmål til TA: Når jeg laver en node "lokalt" i en metode, vil den så forblive når metoden ikke bliver brugt længere?
+            struct node tempNode;
+            tempNode.nodeCard=&cardArray[i];
+            tempNode.nextPointer=NULL;
+            tempNode.previousPointer=NULL;
+            headArray[i]=&tempNode;
+        }
+        for (int i = 0; i < 7; ++i) {
+            struct node tempNode;
+            tempNode.nodeCard[i+7];
+        }
 
-            if(tempNextPointer[i]!=NULL){
+}
+
+
+    void printCardDeck(){
+
+        bool nextPointer=true;
+        char printCards[2][7];
+        struct node tempCurrentNode[7];
+
+        for (int i = 0; i < 7; ++i) {
+            //TODO Skal tjekke om den rent faktisk giver pointeren
+            //Her sørger vi for at den første pointer vi starter med peger på HEAD noden
+            tempCurrentNode[i]=*headArray[i];
+        }
+
+
+        printf("C1\tC2\tC3\tC4\tC5\tC6\tC7");
+        printf("\n");
+
+
+    while(nextPointer){
+
+
+        for (int i = 0; i < 7; ++i) {
+            if(tempCurrentNode[i].nextPointer != NULL){
                 //TODO Tjek om der er styr på pointers
-                struct card tempCard = tempNextPointer[i]->nodeCard;
+                struct card tempCard = *tempCurrentNode[i].nodeCard;
                 char tempCardSuit = tempCard.suit;
                 int tempCardRank = tempCard.rank;
                 bool tempCardIsFaceUp = tempCard.isFaceUp;
@@ -81,7 +132,9 @@ _Noreturn void print(){
 
         }
         printf("");
+        nextPointer=false;
     }
+
 
 
 
